8dd1ba2886ddfba2720c8bab0529920077b0921e
Franz Knupfer <franzknupfer@hotmail.com>
2015-11-21 19:35:31 -0800

    add validation to verify address is in Portland

--- app/models/mural.rb
+++ app/models/mural.rb
@@ -7,6 +7,16 @@ class Mural < ActiveRecord::Base
   validates :region, :presence => true
   validates :attached_image, attachment_presence: true
   validates_with AttachmentPresenceValidator, attributes: :attached_image
+  before_save :portland_address
+
+  def portland_address
+    if (self.latitude < 45.433318) || (self.latitude > 45.651172) || (self.longitude < -122.836649) || (self.longitude > -122.472813)
+      errors.add(:mural_address, "This is not a valid Portland address. Please enter a Portland address.")
+      false
+    else
+      true
+    end
+  end
 
   geocoded_by :mural_address
   after_validation :geocode
