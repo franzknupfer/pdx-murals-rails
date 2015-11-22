c4109883b03303c441d2707b7160d91b2584e657
Franz Knupfer <franzknupfer@hotmail.com>
2015-11-21 19:36:13 -0800

    make infowindow linkable to full mural record

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,10 +5,8 @@ class MapController < ApplicationController
       marker.lat mural.latitude
       marker.lng mural.longitude
       marker.infowindow %Q(<div id="infobox">
-
-      <a href= "#{mural.attached_image.url}">
-
-      <img src="#{mural.attached_image.url(:medium)}"></a><h1>#{mural.mural_name}</h1><h3>#{mural.artist_name}</h3></div>)
+      <a href="/murals/#{mural.id}">
+      <img src="#{mural.attached_image.url(:medium)}"><h1>#{mural.mural_name}</h1><h3>#{mural.artist_name}</h3></a></div>)
     end
   end
 end
