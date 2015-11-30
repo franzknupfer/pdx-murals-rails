f77e45b0796db4f1730f3069cb39ad4d4b6462af
Franz Knupfer <franzknupfer@hotmail.com>
2015-11-29 18:29:59 -0800

    add basic method to mural model to ensure geocoded address is in Portland

--- app/models/mural.rb
+++ app/models/mural.rb
@@ -7,18 +7,12 @@ class Mural < ActiveRecord::Base
   validates :region, :presence => true
   validates :attached_image, attachment_presence: true
   validates_with AttachmentPresenceValidator, attributes: :attached_image
-  before_save :portland_address
 
-  def portland_address
-    if (self.latitude < 45.433318) || (self.latitude > 45.651172) || (self.longitude < -122.836649) || (self.longitude > -122.472813)
-      errors.add(:mural_address, "This is not a valid Portland address. Please enter a Portland address.")
-      false
-    else
-      true
-    end
+  def full_address
+    "#{mural_address}, Portland, OR"
   end
 
-  geocoded_by :mural_address
+  geocoded_by :full_address
   after_validation :geocode
 
   has_attached_file :attached_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
