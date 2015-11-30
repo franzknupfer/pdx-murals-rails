d3531b242ddea7ede1116903ccfb4ccbe787df30
Franz Knupfer <franzknupfer@hotmail.com>
2015-11-30 01:36:08 -0800

    add custom map marker to map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,6 +4,11 @@ class MapController < ApplicationController
     @hash = Gmaps4rails.build_markers(@murals) do |mural, marker|
       marker.lat mural.latitude
       marker.lng mural.longitude
+      marker.picture({
+        url: "#{view_context.image_path("map_marker.png") }",
+                    width: "64",
+                    height: "64"
+      })
       marker.infowindow %Q(<div id="infobox">
       <a href="/murals/#{mural.id}">
       <img src="#{mural.attached_image.url(:medium)}"><h1>#{mural.mural_name}</h1><h3>#{mural.artist_name}</h3></a></div>)
